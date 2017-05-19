3e342bf5b3399229add214b4d92325d88d626514
arguskos <mark.furman@student.howest.be>
2017-05-19 16:44:05 +0400

    new scenes with beats

---
+++ Assets/Scripts/BeatDetectionExample/UpBeat.cs
@@ -0,0 +1,48 @@
+﻿using SynchronizerData;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpBeat : MonoBehaviour {
+
+	public Vector3[] beatPositions;
+
+	private BeatObserver beatObserver;
+	private int beatCounter;
+
+	private float _toleranceTime = 0.2f;
+	public float _toleraceTimer = 0;
+	public bool _canBeHitted = false;
+	void Start()
+	{
+		beatObserver = GetComponent<BeatObserver>();
+		beatCounter = 0;
+	}
+
+	void Update()
+	{
+		if (_canBeHitted)
+		{
+			_toleraceTimer += Time.deltaTime;
+		}
+		if (_toleraceTimer > _toleranceTime)
+		{
+			_toleraceTimer = 0;
+			_canBeHitted = false;
+			GetComponent<Renderer>().material.color = Color.blue;
+		}
+		if ((beatObserver.beatMask & BeatType.UpBeat) == BeatType.UpBeat)
+		{
+			transform.position = beatPositions[beatCounter];
+			beatCounter = (++beatCounter == beatPositions.Length ? 0 : beatCounter);
+
+			_canBeHitted = true;
+			GetComponent<Renderer>().material.color = Color.red;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : GameManagerBase {
+
+	public AudioSource Source;
+	// Use this for initialization
+	new void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	new void Update () {
+		base.Update();
+		
+		StartCoroutine(Helpers.SlowPrint(GetParameter(Parameters.GetParameter(Parameters.ParamsName.SongTempo)).ToString(), 1f));
+		Source.pitch = GetParameter(Parameters.GetParameter(Parameters.ParamsName.SongTempo));
+	}
+}
